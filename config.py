655a88f3a4cd5e4087701be9215d98bb0c60055a
Northa <Northa@users.noreply.github.com>
2021-11-29 17:38:14 +0300

    update config vars

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ config = {
 #     'VALIDATOR_ADDR': '',                 # required
 #     'VALOPER': '',                        # required
 #     'DELEGATOR': '',                      # required
-#     'PROMETHEUS': 'http://8.8.8.8:9100',  # optional
+#     'NODE_EXPORTER_URL': 'http://8.8.8.8:9100',  # optional
 #     'TELEGRAM_TOKEN': '',                 # optional
 #     'TELEGRAM_CHAT_ID': '',               # optional
 #     'ETH_ADDRESS': ''                     # optional
@@ -30,7 +30,7 @@ config = {
 #     'VALIDATOR_ADDR': '088DAD6BD2513F5DFBB2A18C5C40669081F97B97',     # required
 #     'VALOPER': 'umeevaloper1davz40kat93t49ljrkmkl5uqhqq45e0t64rfgg',  # required
 #     'DELEGATOR': 'umee1davz40kat93t49ljrkmkl5uqhqq45e0t63yxez',       # required
-#     'PROMETHEUS': 'http://8.8.8.8:9100',                              # optional
+#     'NODE_EXPORTER_URL': 'http://8.8.8.8:9100',                              # optional
 #     'TELEGRAM_TOKEN': '2087059219:AAEqF6777Wptha3Auq-93tQu_GHs',      # optional
 #     'TELEGRAM_CHAT_ID': '1997773142',                                 # optional
 #     'ETH_ADDRESS': '0xFd662Bb8aEa2C35eFe9135749727C58e6218112e'       # optional
